111a879cb266ccab65a3c755587cba5fc383c57f
sieun <qkttldms0013@gmail.com>
2024-03-27 12:53:44 +0900

    20240327

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 // Express 모듈을 불러옵니다.
 const express = require('express');
 
@@ -7,12 +8,26 @@ const app = express();
 // 기본 포트를 설정하거나 3000 포트를 사용합니다.
 const PORT = process.env.PORT || 3000;
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
 
 // 루트 경로 ('/')에 대한 GET 요청을 처리합니다.
 app.get('/', (req, res) => {
   res.send('Hello, World!!!');
 });
+
+app.post('/test', (req, res) => {
+  console.log("테스트로 들어왔어요.")
+  res.send('Hello, test');
+});
+
+function helloHandler(req, res) {
+  console.log("헬로 핸들러 입니다.");
+  res.send('응답')
+}
+
+app.get('/hello', helloHandler);
 
 // 서버를 설정한 포트에서 실행합니다.
 app.listen(PORT, () => {
